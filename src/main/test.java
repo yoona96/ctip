1e5f65b1bbef8c88b767f5c82e262f0c93c3da72
yoona96 <40069083+yoona96@users.noreply.github.com>
2020-05-21 10:59:03 +0900

    new_test
    blah

--- src/main/test.java
+++ src/main/test.java
@@ -1,34 +1,5 @@
-import java.util.*;
-
-public class SimpleCalculator {
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("연산을 입력하세요: ");
-        String operand = input.next();
-
-        System.out.print("숫자 2개를 입력하세요: ");
-        double num1=input.nextDouble();
-        double num2=input.nextDouble();
-
-        if(operand=="+") {
-            System.out.println(num1+ "+" + num2+ "=" +(num1+num2));
-        }
-        else if(operand=="-") {
-            System.out.println(num1+ "-" + num2+ "=" +(num1-num2));
-        }
-        else if(operand=="*") {
-            System.out.println(num1+ "*" + num2+ "=" +(num1*num2));
-        }
-        else if(operand=="/") {
-            if (num2==0) {
-                System.out.println("0을 나눌 수는 없습니다");
-            }
-            else{
-                System.out.println(num1+ "/" + num2+ "=" +(num1/num2));
-            }
-        }
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
     }
-
 }
